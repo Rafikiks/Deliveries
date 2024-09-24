1ebd9046985df405dd24ee3274d8bd52c33d7d0a
Rafikiks <kylian.razafitsalama@epitech.digital>
2024-09-24 15:38:38 +0200

    Create ex_04correction.js

---
+++ ex_04correction.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title></title>
+</head>
+<body>
+  <h1>Welcome to the javascript pool!</h1>
+  <h2>Have some fun and dont forget to bring a towel.
+  </h2>
+  
+</body>
+</html>
+
+console.log("Website launched!")
